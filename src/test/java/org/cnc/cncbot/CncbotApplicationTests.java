0ae45c39e3c26b9f3dc5ef19b6f260f8b270c062
SebastienH <sebastien.heuze@capgemini.com>
2018-06-15 16:55:58 +0200

    SpringBoot Initialisation

---
+++ src/test/java/org/cnc/cncbot/CncbotApplicationTests.java
@@ -0,0 +1,16 @@
+package org.cnc.cncbot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CncbotApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
