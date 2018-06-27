c5e71dec4128093aec95eba0e293496f96828cd7
SebHeuze <heuze.seb@gmail.com>
2018-06-27 02:25:05 +0200

    Récupération sessionId ok

--- src/main/java/org/cnc/cncbot/map/service/TiberiumAlliancesService.java
+++ src/main/java/org/cnc/cncbot/map/service/TiberiumAlliancesService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
+import retrofit2.http.Query;
 
 /**
  * EA services class
@@ -15,8 +16,16 @@ import retrofit2.http.Header;
 public interface TiberiumAlliancesService {
 	static final String BASE_URL = "https://www.tiberiumalliances.com/";
 	 
-   
+
+	@GET("fr/login/auth")
+	Call<Void> loginAuth();
+	
 	@GET("fr/game/launch")
 	Call<String> gameLaunch(@Header("Cookie") String cookie);
+	
+	@GET("login_check")
+	Call<String> loginCheck(@Header("Cookie") String cookie,
+			                @Query("code") String code,
+			                @Query("state") String state);
 	
 }
---
+++ src/main/java/org/cnc/cncbot/map/utils/StringConverterFactory.java
@@ -0,0 +1,31 @@
+package org.cnc.cncbot.map.utils;
+
+import java.io.IOException;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import okhttp3.MediaType;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+import retrofit2.Converter;
+import retrofit2.Retrofit;
+
+public final class StringConverterFactory extends Converter.Factory {
+  @Override
+  public Converter<ResponseBody, ?> responseBodyConverter(Type type, Annotation[] annotations,
+      Retrofit retrofit) {
+    return new Converter<ResponseBody, String>() {
+      @Override public String convert(ResponseBody value) throws IOException {
+        return value.string();
+      }
+    };
+  }
+
+  @Override public Converter<?, RequestBody> requestBodyConverter(Type type,
+      Annotation[] parameterAnnotations, Annotation[] methodAnnotations, Retrofit retrofit) {
+    return new Converter<String, RequestBody>() {
+      @Override public RequestBody convert(String value) throws IOException {
+        return RequestBody.create(MediaType.parse("text/plain"), value);
+      }
+    };
+  }
+}
