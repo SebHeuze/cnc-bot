bde6fb0484fd04e11814a23aec7e902e383db523
SebHeuze <heuze.seb@gmail.com>
2018-08-13 17:23:48 +0200

    Archive Query

--- src/main/java/org/cnc/cncbot/stats/dao/AllianceDAO.java
+++ src/main/java/org/cnc/cncbot/stats/dao/AllianceDAO.java
@@ -1,5 +1,7 @@
 package org.cnc.cncbot.stats.dao;
 
+import java.util.Date;
+
 import org.cnc.cncbot.stats.entities.Alliance;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -21,8 +23,8 @@ public interface AllianceDAO extends JpaRepository<Alliance, Long> {
 	public void truncateTable();
 	
 
-	@Query(value = "INSERT INTO alliance_hist (id,nom_alliance,nombre_bases,nombre_joueurs,rang,score,top_score,average_score,total_bases_detruites,bases_oublies_detruites,bases_joueurs_detruites,distance_centre,alliance_description,nb_poi,rank_poi_1,rank_poi_2,rank_poi_3,rank_poi_4,rank_poi_5,rank_poi_6,rank_poi_7,score_poi_1,score_poi_2,score_poi_3,score_poi_4,score_poi_5,score_poi_6,score_poi_7,date) SELECT alliance.*, to_date(?, 'YYYY-MM-DD') FROM alliance", nativeQuery = true)
+	@Query(value = "INSERT INTO alliance_hist (id,nom_alliance,nombre_bases,nombre_joueurs,rang,score,top_score,average_score,total_bases_detruites,bases_oublies_detruites,bases_joueurs_detruites,distance_centre,alliance_description,nb_poi,rank_poi_1,rank_poi_2,rank_poi_3,rank_poi_4,rank_poi_5,rank_poi_6,rank_poi_7,score_poi_1,score_poi_2,score_poi_3,score_poi_4,score_poi_5,score_poi_6,score_poi_7,date) SELECT alliance.*, ?1 FROM alliance", nativeQuery = true)
 	@Transactional
 	@Modifying
-	public void archive();
+	public void archive(Date dateArchive);
 }
